a871d5b505fe5270093da672235d5e2efaf0a853
Mehmet Aslan <mehmetaslan2001@gmail.com>
2023-08-11 10:08:17 +0300

    Second negative test added to Delete request

--- src/test/java/com/insiderApi/crudTests/Put_Test.java
+++ src/test/java/com/insiderApi/crudTests/Put_Test.java
@@ -112,5 +112,34 @@ public class Put_Test extends TestBase {
                         "status", is("available")); // status updated before sold now available
     }
 
+    @DisplayName("Negative PUT to /pet") // status updated as "available" instead of "sold" with invalid ID
+    @Test //This is a bug.According to the Swagger It should be 400 invalid id supplied.
+    // (Because ıd accept int64, but we pass more than that) But We get 200.
+    public void putRequest_WithHamcrest_Pojo2() {
+        Pet pet = new Pet();
+        pet.setId(12345678901234567L);
+        Map<String, Object> ctgry = new HashMap<>();
+        ctgry.put("id",0);
+        ctgry.put("name","dogs");
+        pet.setCategory(ctgry);
+        pet.setName("Fido");
+        List<String> photoUrls = new ArrayList<>();
+        photoUrls.add("string");
+        pet.setPhotoUrls(photoUrls);
+        Map<String, Object> tag =new HashMap<>();
+        tag.put("id",0);
+        tag.put("name","string");
+        List<Map<String,Object>> tags = new ArrayList<>();
+        tags.add(tag);
+        pet.setTags(tags);
+        pet.setStatus("available");
+
+        given().accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .body(pet)
+                .when().put("")
+                .then().statusCode(400).contentType("application/json");
+
+    }
 
 }
